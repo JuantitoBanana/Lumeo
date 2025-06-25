db917f522e76ad0195847a5182bafdab0977358b
JuantitoBanana <juanjoseblancodiaz1504@gmail.com>
2025-06-25 23:59:48 +0200

    Master

---
+++ lumeo_backend/src/test/java/com/lumeo/lumeo_backend/LumeoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumeo.lumeo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumeoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
